5cd8feb01021e661983b182469fff2c5ef509322
ingjorivera <jorivera@gmail.com>
2022-08-04 01:24:19 -0500

    sms

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
-import os
-from typing import Optional
-from fastapi import FastAPI
-import uvicorn
-import random
+import asyncio
+from fastapi import FastAPI, Form, status
+from fastapi.responses import FileResponse, RedirectResponse
+from twilio.rest import Client
+import config
 
 app = FastAPI()
+settings = config.Settings()
 
-@app.get("/")
-def home():
-    return {"Hello": "World from FastAPI"}
 
-# get random number between min(default:0) and max(default:9)
-@app.get("/random/")
-def get_random(min: Optional[int] = 0, max: Optional[int] = 9):
-    rval = random.randint(min, max)
-    return { "value": rval }
+@app.get('/')
+async def index():
+    return FileResponse('index.html')
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=int(os.getenv("PORT", default=5000)), log_level="info")
+@app.post('/')
+async def handle_form(phone: str = Form(...)):
+    await asyncio.get_event_loop().run_in_executor(
+        None, send_sms, phone, 'Hola!, el codigo solicitado es: ')
+    return RedirectResponse('/success', status_code=status.HTTP_303_SEE_OTHER)
+
+
+def send_sms(to_number, body):
+    client = Client(settings.twilio_account_sid, settings.twilio_auth_token)
+    return client.messages.create(from_=settings.twilio_phone_number,
+                                  to=to_number, body=body)
+
+@app.get('/success')
+async def success():
+    return FileResponse('success.html')
